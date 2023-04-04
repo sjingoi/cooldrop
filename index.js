681fad56cef3c4616c9402ec01a87defbcb3d512
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-04-03 23:49:35 -0400

    Added file transfer

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ sendBox = document.getElementById("chat-box-send")
 recieveBox = document.getElementById("chat-box-recieve")
 connectionMsg = document.getElementById("connection-open-msg")
 
+//FILE TRANSFER
+fileInput = document.getElementById("fileInput");
+fileUploadBtn = document.getElementById("send-file")
+
+var chunkcount = 0;
+var chunks = [];
+
 const SERVERS = {
     iceServers:[
         {
@@ -47,10 +54,13 @@ function createLocalConnection() {
 
     localOfferBox.disabled = false
 
-    dc.onmessage = message => recieveBox.textContent = message.data
+    dc.onmessage = message => {
+        dataHandler(message);
+    }
     dc.onopen = e => {
         console.log("Connection opened.")
         startChatBox(dc);
+        startFileBox(dc);
     };
     lc.onicecandidate = e => {
         console.log("Created new ICE candidate.")
@@ -77,13 +87,40 @@ function createRemoteConnection() {
     }
     rc.ondatachannel = dataChannel => {
         rc.dc = dataChannel.channel
-        rc.dc.onmessage = message => recieveBox.textContent = message.data;
+        rc.dc.onmessage = message => {
+            dataHandler(message);
+        }
         rc.dc.onopen = e => console.log("Connection opened.")
         startChatBox(rc.dc);
+        startFileBox(rc.dc);
     }
     rc.setRemoteDescription(offer).then(a => console.log("Offer set."));
     rc.createAnswer().then(answer => rc.setLocalDescription(answer)).then(a => console.log("Answer created."))
     
+}
+
+function dataHandler(message) {
+    console.log("Type of data: " + typeof(message.data));
+    if (typeof(message.data) == "string") {
+        if (message.data.startsWith("%/chunkcount")) {
+            chunks = [];
+            chunkcount = parseInt(message.data.replace("%/chunkcount", ""));
+            console.log("Hello123 " + chunkcount)
+        }
+        console.log("Hello");
+        recieveBox.textContent = message.data
+        console.log("Hello123 " + chunkcount)
+    } else if (typeof(message.data) == "object") {
+        console.log(chunks.length);
+        //console.log(chunkcount);
+        const chunk = message.data;
+        chunks.push(chunk);
+        if (chunks.length == chunkcount) {
+            console.log("Hello2");
+            fileData = new Blob(chunks, { type: 'application/octet-stream' });
+            downloadBlobAsFile(fileData, "test")
+        }
+    }
 }
 
 function startChatBox(dataChannel) {
@@ -97,3 +134,55 @@ function startChatBox(dataChannel) {
         sendBox.value = "";
     }
 }
+
+function startFileBox(dataChannel) {
+    fileUploadBtn.onclick = () => {
+        const file = fileInput.files[0];
+        const chunkSize = 64 * 1024;
+        const numChunks = Math.ceil(file.size / chunkSize);
+        let offset = 0;
+        dataChannel.send("%/chunkcount" + numChunks);
+        for (let i = 0; i < numChunks; i++) {
+            const chunk = file.slice(offset, offset + chunkSize);
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                dataChannel.send(event.target.result);
+            }
+            reader.readAsArrayBuffer(chunk);
+            offset += chunkSize;
+        }
+    }
+}
+
+function uploadFile() {
+    const file = fileInput.files[0];
+
+    
+  
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const contents = event.target.result;
+      // Do something with the file contents...
+      console.log(contents);
+    };
+    reader.readAsArrayBuffer(file);
+}
+
+function downloadBlobAsFile(blob) {
+    // Get the filename from the Blob object
+    const filename = blob.name;
+  
+    // Create a new download link element
+    const downloadLink = document.createElement('a');
+  
+    // Set the download link attributes
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = filename;
+  
+    // Simulate a click on the download link
+    downloadLink.click();
+  
+    // Clean up the URL object
+    URL.revokeObjectURL(downloadLink.href);
+  }
+  
