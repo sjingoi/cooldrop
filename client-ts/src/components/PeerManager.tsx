8cfd51053a974188a42630f351dbdb6a1c2274d7
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-06-17 16:25:48 -0400

    uuid generates client-side; removed connection id

--- client-ts/src/components/PeerManager.tsx
+++ client-ts/src/components/PeerManager.tsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react';
 import socket from '../scripts/socket';
 import PeerConnection, { Package } from '../scripts/peer-connection';
 import Peer from './Peer';
-import './peermanager.css'
+import { v4 as uuidv4 } from 'uuid';
+import './peermanager.css';
 
 var local_uuid: string;
 var connections: PeerConnection[] = [];
@@ -10,16 +11,28 @@ var connections: PeerConnection[] = [];
 function PeerManager() {
     const [isConnected, setIsConnected] = useState(socket.connected);
     const [peers, setPeers] = useState<PeerConnection[]>([]);
+
     //console.log("New render")
-    console.log(peers);
+    //console.log(peers);
     
 
     useEffect(() => {
-        console.log("Rendered");
+        let uuid = localStorage.getItem("uuid");
+        if (uuid === null) {
+            local_uuid = uuidv4();
+            localStorage.setItem("uuid", local_uuid);
+        } else {
+            local_uuid = uuid;
+        }
 
-        function getPeer(connection_id: string) {
+        socket.emit("init", JSON.stringify({
+            "uuid": uuid,
+            "name": "test",
+        }));
+
+        function getPeer(peer_id: string) {
             for (let i = 0; i < connections.length; i++) {
-                if (connections[i].connection_id == connection_id) {
+                if (connections[i].get_remote_id() == peer_id) {
                     return connections[i];
                 }
             }
@@ -58,9 +71,9 @@ function PeerManager() {
                     break;
         
                 case 'sdp':
-                    const peer = getPeer(msg.connection_id)
+                    const peer = getPeer(msg.sender)
                     if (peer !== undefined) {
-                        console.log("Setting remote for " + peer.connection_id)
+                        console.log("Setting remote for " + peer.get_remote_id())
                         peer.set_remote(msg.sdp);
                     } else {
                         createConnection(msg);
@@ -68,10 +81,30 @@ function PeerManager() {
                     break;
             }
         }
+        function onSDP(message: any) {
+            console.log("Recieved SDP")
+            var msg = JSON.parse(message);
+            const peer = getPeer(msg.sender)
+            if (peer !== undefined) {
+                console.log("Setting remote for " + peer.get_remote_id())
+                peer.set_remote(msg.sdp);
+            } else {
+                createConnection(msg);
+            }
+        }
+
+        function sendToServer(type: string, pkg: any) {
+            //socket.send(JSON.stringify(pkg))\
+            pkg.sender = local_uuid;
+            socket.emit(type, JSON.stringify(pkg));
+        }
         
         function createConnection(params?: any) {
+            let remote_uuid: string = params.sender;
+            //let connection_id: string = params.connection_id;
+            let sdp: RTCSessionDescription = params.sdp;
             
-            let new_peer: PeerConnection = new PeerConnection(params.connection_id, params.recipient, params.sender, (pkg: Package) => socket.send(JSON.stringify(pkg)), params.sdp);
+            let new_peer: PeerConnection = new PeerConnection(remote_uuid, sendToServer, sdp);
             
             new_peer.on_open = () => {
                 setPeers(prevPeers => {
@@ -80,21 +113,23 @@ function PeerManager() {
             }
             new_peer.on_close = () => {
                 setPeers(prevPeers => {
-                    return prevPeers .filter(peer => peer.connection_id !== new_peer.connection_id);
+                    return prevPeers.filter(peer => peer.get_remote_id() !== new_peer.get_remote_id());
                 });
             }
             connections.push(new_peer);
-            console.log("Creating peer with id " + new_peer.get_remote_id())
+            //console.log("Creating peer with id " + new_peer.get_remote_id())
         }
 
         socket.on("connect", () => onConnect());
         socket.on("disconnect", () => onDisconnect());
         socket.on("message", message => onMessage(message));
+        socket.on("sdp", (message: any) => onSDP(message));
 
         return () => {
             socket.off("connect");
             socket.off("disconnect");
             socket.off("message");
+            socket.off("sdp");
         }
     }, [])
 
@@ -103,11 +138,10 @@ function PeerManager() {
             <p>Connection Status: </p>
             {isConnected ? <p style={{color: "green"}}>Connected</p> : <p style={{color: "red"}}>Disconnected</p>}
             <p>Your ID: {local_uuid}</p>
-            <h1>Peers:</h1>
             <ul className='peer-list'>
                 {peers.length === 0 && <p style={{marginTop: "8%"}}>No peers are currently connected. Open CoolDrop on another device, or wait for others to join.</p>}
                 {peers.map(peer => (
-                    <li key={peer.connection_id}>
+                    <li key={peer.get_remote_id()}>
                         <Peer peer={peer} id={peer.get_remote_id()}/>
                     </li>
                 ))}
