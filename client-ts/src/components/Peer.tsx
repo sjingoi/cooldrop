8ce418a19c297ba1f9dac8eb82372eb59f23e6a4
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-06-20 19:25:54 -0400

    checked for empty file before peer.send_file

--- client-ts/src/components/Peer.tsx
+++ client-ts/src/components/Peer.tsx
@@ -30,8 +30,8 @@ function Peer (props: Props) {
             return
         }
         console.log("Sending file to peer id: " + peer.get_remote_id());
-        
-        if (files) {peer.send_file(files[0])}
+        console.log(files);
+        if (files && files[0]) {peer.send_file(files[0])}
     }
 
     const onDragEnter = () => {
@@ -57,7 +57,7 @@ function Peer (props: Props) {
             <img src={computerIcon} className='icon'></img>
             <progress value={progress} className='progress'></progress>
             <label htmlFor={"file-" + id} className='input-file' ref={wrapperRef} onDragEnter={onDragEnter} onDragLeave={onDragLeave} onDrop={e => onDrop(e)} onDragOver={(event) => event.preventDefault()}>CHOOSE FILE</label>
-            <input type="file" ref={fileBoxRef} id={"file-" + id} onChange={() => {if (fileBoxRef.current) handleSendFile(fileBoxRef.current.files)}}></input>
+            <input type="file" ref={fileBoxRef} id={"file-" + id} onChange={() => {if (fileBoxRef.current) handleSendFile(fileBoxRef.current.files)}} multiple></input>
         </div>
     )
 }
